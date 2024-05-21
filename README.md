2def4e0d4d9e546b4d445c25a8e61f5f6e443d26
Yeffian <aditchakraborty2009@gmail.com>
2024-05-21 19:58:38 +0800

    inital commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# lang-percent
+
+Measure the ratio of different languages/tools used in your project.
+
+## Usage
+
+You can run it by using:
+```
+npx lang-percent <ignore_modules>
+```
+All folder names supplied after the command will be ignored in the search.
+
+## Language support
+Since I'm manually reading from a .json file for extensions, this is all we support currently, but feel free to add a pull request with better ways to do it, or just more. The extensions we support currently are:
+- JSON
+- Typescript
+- Javascript
+- Python
+- C#
+- C
+- C++
+- HTML
+- CSS
+- Ruby
+- Golang
+- Java
+- Swift
+- Kotlin
+- Groovy
+- Bash Script
---
+++ src/main.js
@@ -0,0 +1,67 @@
+#! /usr/bin/env node
+import figlet from 'figlet';
+import gradient from 'gradient-string'
+import chalk from 'chalk';
+import fs from 'fs';
+import languages from './languages.json' with { type: "json"};
+
+const write = console.log;
+
+function traverseFiles(dirPath, ignoreFolders) {
+  const allFiles = []; 
+
+  try {
+    const files = fs.readdirSync(dirPath);
+  
+    for (const file of files) {
+      const filePath = `${dirPath}/${file}`;
+      const stats = fs.statSync(filePath);
+
+      if (stats.isFile()) {
+        allFiles.push({ name: file, ext: file.split(".")[1]});
+      }
+
+      if (stats.isDirectory() && !ignoreFolders.includes(file)) {
+        const subfolderFiles = traverseFiles(filePath, ignoreFolders);
+        allFiles.push(...subfolderFiles);
+      }
+    }
+
+    return allFiles;
+  } catch (err) {
+    console.error('Error reading directory:', err);
+  }
+
+  return undefined;
+}
+
+figlet("lang-percent", (err, text) => {
+  const foldersToIgnore = process.argv.slice(2);
+  const extensionCounts = {};
+  write(gradient.pastel.multiline(text));  
+  
+  const files = traverseFiles(process.cwd(), foldersToIgnore);  
+  const numberOfFiles = files.length;
+  for (var file of files) {
+    const ext = file['ext'].toLowerCase();
+
+    if (extensionCounts.hasOwnProperty(ext)) {
+      extensionCounts[ext] = extensionCounts[ext] + 1;
+    } else {
+      extensionCounts[ext] = 1;
+    }
+  }
+
+  write(chalk.blue.bgRed.bold('Your project has..'))
+
+  let output = '';
+
+  for (var extension in extensionCounts) {
+    const language = languages[extension];
+    const percentage = Math.ceil(extensionCounts[extension] / numberOfFiles * 100);
+
+    output += `${language}: ${percentage}\n`;
+  }
+
+  write(gradient.instagram.multiline(output));
+});
